9d397cb799b59e7e808d83890a20a0f03be899ea
comanezz <aymeric.lcharles@gmail.com>
2020-04-03 02:00:31 +0100

    Allowed host added

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'lc(_0swa5%f$%_1i8+^t(4lg_di90$6dfx=!1j$#vfl%1v_kvg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simple-django-first-todo.herokuapp.com']
 
 
 # Application definition
@@ -123,10 +123,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
